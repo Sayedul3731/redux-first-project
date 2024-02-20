f59b2f35d18ebdcb7ab194eb5f294770fb951da4
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-20 12:18:15 +0600

    reducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Counter from "./components/Counter"
 
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl text-center font-bold py-5">Redux First Project</h1>
+      <Counter/>
     </>
   )
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
 
 
 const Counter = () => {
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch()
     return (
         <div>
-            <h1>Counter</h1>
-        </div>
+      <div className=" text-center mt-20">
+        <button
+        className="bg-green-500 px-2 font-semibold text-white text-2xl py-2"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span className="mx-10 text-2xl bg-blue-500 px-2 text-white font-semibold ">{count}</span>
+        <button
+        className="bg-green-500 px-2 font-semibold text-white text-2xl py-2"
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
     );
 };
 
